ba8eb986d31f540c4da976df9f9c281dc55dbe2f
alexey_chupin <a.chupin@etf-int.ru>
2019-10-09 17:56:23 +0300

    serializer added, then refactored

--- snippets/models.py
+++ snippets/models.py
@@ -14,7 +14,10 @@ class Snippet (models.Model):
     code = models.TextField()
     linenos = models.BooleanField(default=False)
     language = models.CharField(choices=LANGUAGE_CHOICES, default='python', max_length=100)
-    style = models.CharField()
+    style = models.CharField(STYLE_CHOICES, default='friendly', max_length=100)
+
+    class Meta:
+        ordering = ['created']
 
 
 
